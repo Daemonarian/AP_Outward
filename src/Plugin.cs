318830e2fe22426e8414c0b42258ad47feb2f648
Daemonarian <daemonarian@gmail.com>
2025-11-11 04:32:47 -0500

    spawned bird mask in Cierzo lighthouse

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -3,28 +3,29 @@ using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
-// RENAME 'OutwardModTemplate' TO SOMETHING ELSE
-namespace OutwardModTemplate
+
+namespace OutwardArchipelago
 {
     [BepInPlugin(GUID, NAME, VERSION)]
     public class Plugin : BaseUnityPlugin
     {
         // Choose a GUID for your project. Change "myname" and "mymod".
-        public const string GUID = "myname.mymod";
+        public const string GUID = "apoutward.daemonarium.com";
         // Choose a NAME for your project, generally the same as your Assembly Name.
-        public const string NAME = "My Mod";
+        public const string NAME = "Outward Archipelago";
         // Increment the VERSION when you release a new version of your mod.
         public const string VERSION = "1.0.0";
 
         // For accessing your BepInEx Logger from outside of this class (eg Plugin.Log.LogMessage("");)
         internal static ManualLogSource Log;
-
-        // If you need settings, define them like so:
-        public static ConfigEntry<bool> ExampleConfig;
 
         // Awake is called when your plugin is created. Use this to set up your mod.
         internal void Awake()
@@ -32,18 +33,51 @@ namespace OutwardModTemplate
             Log = this.Logger;
             Log.LogMessage($"Hello world from {NAME} {VERSION}!");
 
-            // Any config settings you define should be set up like this:
-            ExampleConfig = Config.Bind("ExampleCategory", "ExampleSetting", false, "This is an example setting.");
-
             // Harmony is for patching methods. If you're not patching anything, you can comment-out or delete this line.
             new Harmony(GUID).PatchAll();
+
+            SceneManager.sceneLoaded += new UnityAction<Scene, LoadSceneMode>(OnSceneLoaded);
         }
 
-        // Update is called once per frame. Use this only if needed.
-        // You also have all other MonoBehaviour methods available (OnGUI, etc)
-        internal void Update()
+        internal void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
+            if (scene.name.ToLower().Contains("lowmemory") || scene.name.ToLower().Contains("mainmenu"))
+            {
+                return;
+            }
 
+            Log.LogMessage($"Scene loaded: {scene.name}");
+
+            this.StartCoroutine(WaitForSceneReady());
+        }
+
+        private IEnumerator WaitForSceneReady()
+        {
+            while (!NetworkLevelLoader.Instance.IsOverallLoadingDone || !NetworkLevelLoader.Instance.AllPlayerDoneLoading)
+            {
+                yield return null;
+            }
+
+            Log.LogMessage("Scene ready");
+
+            this.StartCoroutine(WaitForGameplayResumed());
+        }
+
+        private IEnumerator WaitForGameplayResumed()
+        {
+            while (NetworkLevelLoader.Instance.IsGameplayPaused)
+            {
+                yield return null;
+            }
+
+            Log.LogMessage("Gameplay has resumed");
+
+            if (SceneManagerHelper.ActiveSceneName == "CierzoNewTerrain" && !PhotonNetwork.isNonMasterClientInRoom)
+            {
+                Log.LogMessage("CierzoNewTerrain detected, spawning bird mask.");
+                Item birdMask = ItemManager.Instance.GenerateItemNetwork(3000250);
+                birdMask.ChangeParent(null, new UnityEngine.Vector3(-172.2164f, -1500.047f, 759.5696f), UnityEngine.Quaternion.identity);
+            }
         }
 
         // This is an example of a Harmony patch.
@@ -56,6 +90,7 @@ namespace OutwardModTemplate
                 // This is a "Postfix" (runs after original) on ResourcesPrefabManager.Load
                 // For more documentation on Harmony, see the Harmony Wiki.
                 // https://harmony.pardeike.net/
+                Plugin.Log.LogMessage("ResourcesPrefabManager.Load was called.");
             }
         }
     }
