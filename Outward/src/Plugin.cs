66bb62fc48bdc552df72106a5305a75113282138
Daemonarian <daemonarian@gmail.com>
2026-01-10 02:33:31 -0500

    added localization support for new dialogue

--- Outward/src/Plugin.cs
+++ Outward/src/Plugin.cs
@@ -108,32 +108,5 @@ namespace OutwardArchipelago
             Log.LogError($"Could not find asset at: {path}");
             return null;
         }
-
-        [HarmonyPatch(typeof(DialogueTree), nameof(DialogueTree.OnGraphStarted))]
-        public class DialogueTree_OnGraphStarted
-        {
-            static void Prefix(DialogueTree __instance)
-            {
-                if (__instance.name == "Dialogue_RissaAberdeen_Neut_Prequest")
-                {
-                    if (__instance.primeNode as ConditionNode == null || (__instance.primeNode as ConditionNode).condition as Condition_QuestLicense == null)
-                    {
-                        var originalStartNode = __instance.primeNode;
-
-                        var gateNode = __instance.AddNode<ConditionNode>();
-                        gateNode.condition = new Condition_QuestLicense(1);
-
-                        var rejectNode = __instance.AddNode<StatementNodeExt>();
-                        rejectNode.actorName = __instance.actorParameters.FirstOrDefault()?.name ?? "Speaker";
-                        rejectNode.statement = new Statement("Come back to me when you have Quest License 1.");
-
-                        Connection.Create(gateNode, originalStartNode);
-                        Connection.Create(gateNode, rejectNode);
-
-                        __instance.primeNode = gateNode;
-                    }
-                }
-            }
-        }
     }
 }
---
+++ Outward/src/QuestLicenseDialogueManager.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using NodeCanvas.DialogueTrees;
+using NodeCanvas.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutwardArchipelago
+{
+    public static class QuestLicenseDialogueManager
+    {
+
+
+        [HarmonyPatch(typeof(DialogueTree), nameof(DialogueTree.OnGraphStarted))]
+        public class DialogueTree_OnGraphStarted
+        {
+            static void Prefix(DialogueTree __instance)
+            {
+                if (__instance.name == "Dialogue_RissaAberdeen_Neut_Prequest")
+                {
+                    if (__instance.primeNode as ConditionNode == null || (__instance.primeNode as ConditionNode).condition as Condition_QuestLicense == null)
+                    {
+                        var originalStartNode = __instance.primeNode;
+
+                        var gateNode = __instance.AddNode<ConditionNode>();
+                        gateNode.condition = new Condition_QuestLicense(1);
+
+                        string text = "";
+                        if (!LocalizationManager.Instance.TryGetLoc($"{Plugin.GUID}.dialogue.rissa.quest_license_1_required", out text))
+                        {
+                            text = $"[LOC] {Plugin.GUID}.dialogue.rissa.quest_license_1_required";
+                        }
+
+                        var rejectNode = __instance.AddNode<StatementNodeExt>();
+                        rejectNode.actorName = __instance.actorParameters.FirstOrDefault()?.name ?? "Speaker";
+                        rejectNode.statement = new Statement(text);
+
+                        Connection.Create(gateNode, originalStartNode);
+                        Connection.Create(gateNode, rejectNode);
+
+                        __instance.primeNode = gateNode;
+                    }
+                }
+            }
+        }
+    }
+}
